91707cb81829af758d285eeb974e46a604992d00
Javier Alba <javieralba027@gmail.com>
2026-03-13 12:26:13 -0500

    RouterLinks - Desde componentes hijos

--- src/app/gifs/components/side-menu/side-menu-options/side-menu-options.ts
+++ src/app/gifs/components/side-menu/side-menu-options/side-menu-options.ts
@@ -1,8 +1,38 @@
 import { Component } from '@angular/core';
+import { NgClass } from "../../../../../../node_modules/@angular/common/types/_common_module-chunk";
+import { RouterLink, RouterLinkActive } from "@angular/router";
+
+// Creando Interface del menu de opciones
+
+interface MenuOption {
+  icon: string;
+  label: string;
+  route: string;
+  sublabel: string;
+}
 
 @Component({
   selector: 'gifs-side-menu-options',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './side-menu-options.html',
 })
-export class SideMenuOptions { }
+export class SideMenuOptions {
+
+  // property  menuOptions en el cual va se un arreglo de tipo MenuOption[] se vincula a la interface MenuOption
+  // dentro contiene objectos {}
+
+  menuOptions:MenuOption[] = [
+    {
+      icon: 'fa-solid fa-chart-line',
+      label: 'Trending',
+      sublabel: 'Gifs Populares',
+      route: '/dashboard/trending'
+    },
+    {
+      icon: 'fa-solid fa-magnifying-glass',
+      label: 'Buscador',
+      sublabel: 'Buscar gifs',
+      route: '/dashboard/search'
+    },
+  ]
+}
