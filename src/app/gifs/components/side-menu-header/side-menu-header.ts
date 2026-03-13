0ec8f26e7683f6216d60f1d44c639b90426bc61e
Javier Alba <javieralba027@gmail.com>
2026-03-13 12:54:31 -0500

    RouterLinks - Desde componentes hijos - Actualizado

--- src/app/gifs/components/side-menu-header/side-menu-header.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  // El nombre del selectro en como podemos llamarlo en el .html padre de este manera "selector: 'gifs-side-menu-header'"
-  selector: 'gifs-side-menu-header',
-  imports: [],
-  templateUrl: './side-menu-header.html',
-})
-export class SideMenuHeader { }
--- src/app/gifs/components/side-menu/side-menu-options/side-menu-options.ts
+++ src/app/gifs/components/side-menu/side-menu-options/side-menu-options.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { NgClass } from "../../../../../../node_modules/@angular/common/types/_common_module-chunk";
 import { RouterLink, RouterLinkActive } from "@angular/router";
 
 // Creando Interface del menu de opciones
