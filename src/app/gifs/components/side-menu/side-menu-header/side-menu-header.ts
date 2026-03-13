c6bfe5abe437bbb65d259fe81045e017c1122724
Javier Alba <javieralba027@gmail.com>
2026-03-13 14:42:13 -0500

    Angular Environments y Path Alias

--- src/app/gifs/components/side-menu/side-menu-header/side-menu-header.ts
+++ src/app/gifs/components/side-menu/side-menu-header/side-menu-header.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { environment } from '@environments/environment';
+//import { environment } from '../../../../../environments/environment';
+
 
 @Component({
   // El nombre del selectro en como podemos llamarlo en el .html padre de este manera "selector: 'gifs-side-menu-header'"
@@ -6,4 +9,9 @@ import { Component } from '@angular/core';
   imports: [],
   templateUrl: './side-menu-header.html',
 })
-export class SideMenuHeader { }
+export class SideMenuHeader {
+
+  // Creamos una  Property
+  // tomamos el enviroment de produccion
+  envs = environment
+}
