f92ddddc311db8ded2a3ae9d74cbd7e93bc3e656
Javier Alba <javieralba027@gmail.com>
2026-03-13 17:50:57 -0500

    input de imagenes - Arreglo de imagenes - imageUrls: string[] - Señal gifs = signal(imageUrls)

--- src/app/gifs/components/gif-list/gif-list-item/gif-list-item.ts
+++ src/app/gifs/components/gif-list/gif-list-item/gif-list-item.ts
@@ -1,8 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'gif-list-item',
   imports: [],
   templateUrl: './gif-list-item.html',
 })
-export class GifListItem { }
+export class GifListItem {
+
+  // imgeUrl = input.required<string>() es una signal obligatoria
+  imgeUrl = input.required<string>()
+}
--- src/app/gifs/components/gif-list/gif-list.ts
+++ src/app/gifs/components/gif-list/gif-list.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { GifListItem } from "./gif-list-item/gif-list-item";
 
 @Component({
@@ -6,4 +6,10 @@ import { GifListItem } from "./gif-list-item/gif-list-item";
   imports: [GifListItem],
   templateUrl: './gif-list.html',
 })
-export class GifList { }
+export class GifList {
+
+  //TODO: input string[]
+
+  // gifs es una señal que requerimos como input y  luego le pasamos un arreglo de string
+  gifs = input.required<string[]>();
+}
